2cdc5da209d6f38ad61e7e92d768e8d84f4170b5
Yousef Kakar <113251900+MrSuhSpence@users.noreply.github.com>
2026-06-23 01:17:58 +0000

    tf code take 3

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "YOURBUCKETNAME"
+    bucket  = "one-million1"
     key     = "actionspipeline1000/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
