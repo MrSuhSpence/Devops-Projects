aacd432dd8888524316894abced86f8edba9e902
Yousef Kakar <113251900+MrSuhSpence@users.noreply.github.com>
2026-06-23 01:21:52 +0000

    update

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "one-million1"
     key     = "actionspipeline1000/terraform.tfstate"
-    region  = "us-east-1"
+    region  = "us-east-2"
     encrypt = true
   }
 }
